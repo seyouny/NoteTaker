6cfe27bcf0f9c1d86f209606f7a8678107fe65ac
Seyoung Yoon <stacey.exp@gmail.com>
2020-07-19 23:27:05 -0700

    heroku adjustments

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 7000;
+
+notes = []
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,24 +22,26 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/api/notes", function (req, res) {
-    return res.json(notes);
+    fs.readFile("db/db.json",function(err,data){
+        return res.json(JSON.parse(data));
+    })
 });
 // Create New Characters - takes in JSON input
 app.post("/api/notes", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newcharacter = req.body;
+    var newNote = req.body;
+    
+    console.log(newNote);
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    notes.push(newNote);
   
-    console.log(newcharacter);
-  
-    characters.push(newcharacter);
-  
-    res.json(newcharacter);
+    res.json(newNote);
   });
+
+app.delete("/api/notes/:id", function(req, res){
+
+});
 
 //   * DELETE `/api/notes/:id` - Should recieve a query paramter 
 //   containing the id of a note to delete. This means
